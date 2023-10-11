382d6b341d6231f87eb6d3c45a67f88f3b760d5d
feslipebraga <feslipebraga@gmail.com>
2023-10-11 12:48:03 -0300

    removed java

--- Java/POO Lista 1/Ex1/src/main/java/com/mycompany/ex1/Ex1.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.ex1;
-
-public class Ex1 {
-    public static void main(String[] args) {
-        Bola minhaBola = new Bola("Azul", 0.7f, "Borracha");
-        minhaBola.trocaCor("Preto");
-        System.out.println("Cor da Bola: " + minhaBola.mostraCor());
-    }
-}
